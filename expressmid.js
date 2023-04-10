55f488fc6663e0daabdd68c9acb3b1f862c17c6f
Charlie <meow@charliecat.space>
2023-04-10 02:02:11 +0200

    removing console.log from `expressmid`

--- expressmid.js
+++ expressmid.js
@@ -5,7 +5,7 @@ const {AltSource} = require('./classes')
  * @returns {any}
  */
 module.exports = function (Conf) {
-    console.log(Conf)
+    //console.log(Conf)
     let Alt;
     if (Conf.constructor.name === "AltSource") {
         Alt = Conf
@@ -14,21 +14,21 @@ module.exports = function (Conf) {
     }
 
     return (req, res, next) => {
-        console.log("mid:", req, res, next)
+        //console.log("mid:", req, res, next)
         if (Conf.hasOwnProperty('path')) {
             if (String(req.baseUrl + req.path) === Conf.path) {
-                console.log('path: custom', String(req.baseUrl + req.path));
+                //console.log('path: custom', String(req.baseUrl + req.path));
                 res.json(Alt.toJSON())
             } else {
-                console.log('outside of the scope with path')
+                //console.log('outside of the scope with path')
                 res.altsource = Alt;
                 next(); 
             }
         } else if (!Conf.hasOwnProperty('path') && String(req.baseUrl + req.path) === '/altstore.json') {
-            console.log('path: default', '/altstore.json');
+            //console.log('path: default', '/altstore.json');
             res.json(Alt.toJSON())
         } else {
-            console.log('outside of the scope')
+            //console.log('outside of the scope')
             req.altsource = Alt;
             next(); 
         }
